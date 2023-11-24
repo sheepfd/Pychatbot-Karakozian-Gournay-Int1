1245273bfacb25373292a7fca71dd4a5db08fd63
sheepfd <vrk.karakozian@gmail.com>
2023-11-24 13:48:11 +0100

    Changes by calam

--- Test.py
+++ Test.py
@@ -2,13 +2,12 @@ from functions1 import *
 
 with open('Cleaned/Nomination_Chirac1',"r",encoding="utf-8") as f :
     speech = f.read()
-tf(speech)
+idf_score()
 
-
-"""a = list_of_files("./Speeches","txt")
+a = list_of_files("./Speeches","txt")
 for filename in os.listdir("Speeches"):
     lower_letter(f'Speeches/{filename}') # in order to fill completly the folder Cleaned
-"""
+
 """with open("Speeches/Nomination_Chirac.txt","r") as f:
     content= f.read()
     print(content)"""
