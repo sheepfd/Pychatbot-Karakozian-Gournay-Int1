3d715f6b3f95eecabd208429517e0f4c9216fb7b
Paul Gournay <151144120+PaulGournay@users.noreply.github.com>
2023-11-26 11:34:05 +0100

    Update Features.py

--- Features.py
+++ Features.py
@@ -1,85 +1,92 @@
 from functions1 import *
+feature = int(input("Enter the number of the feature: "))
 #1.
-"""count= 0
-a = Tf_idf("Cleaned")
-least_important = []
-for i in a :
-    for key,value in i.items():
-        if value == 1:
-            least_important.append(key)
-least_important = set(least_important)
-least_important = list(least_important)
-print(least_important) #['mesdames', 'se', 'aux', 'messieurs', 'il', 'faire', 'j', 'france', 'histoire', 'mais', 'son', 'par', 'peuple']"""
+if feature==1:
+    count= 0
+    a = Tf_idf("Cleaned")
+    least_important = []
+    for i in a :
+        for key,value in i.items():
+            if value == 1:
+                least_important.append(key)
+    least_important = set(least_important)
+    least_important = list(least_important)
+    print(least_important) #['mesdames', 'se', 'aux', 'messieurs', 'il', 'faire', 'j', 'france', 'histoire', 'mais', 'son', 'par', 'peuple']"""
 #2.
-"""a = Tf_idf("Cleaned")
-word=tuple()
-max = 0
-for i in a:
-    for key,value in i.items():
-        if value > max:
-            max = value
-            word= (key,value) 
-print(word) # in the output('la', 81.0)"""
+elif feature==2:
+    a = Tf_idf("Cleaned")
+    word=tuple()
+    max = 0
+    for i in a:
+        for key,value in i.items():
+            if value > max:
+                max = value
+                word= (key,value)
+    print(word) # in the output('la', 81.0)"""
 
 #3.
-"""
-max = 0
-word = ()
-with open("Cleaned/Nomination_Chirac1","r",encoding = 'utf-8') as f:
-    speech1 = f.read()
-with open("Cleaned/Nomination_Chirac2","r",encoding = 'utf-8') as f:
-    speech2 = f.read()
-speech = tf(speech1 +speech2)
-for key,value in speech.items():
-    if value > max:
-        max = value
-        word= (key,value)
-    if value == max: #in order to verify if there no other value which equal to the gratest one 
-        print(value)
-print(word) #output ('de', 97)
-"""
-#4
-"""max=0
-count = 0
-president = ()
-for filename in os.listdir("Cleaned"):
-    with open(f"Cleaned/{filename}", "r", encoding="utf-8") as f:
-        speech = f.read()
-    speech = tf(speech)
+elif feature==3:
+    max = 0
+    word = ()
+    with open("Cleaned/Nomination_Chirac1","r",encoding = 'utf-8') as f:
+        speech1 = f.read()
+    with open("Cleaned/Nomination_Chirac2","r",encoding = 'utf-8') as f:
+        speech2 = f.read()
+    speech = tf(speech1 +speech2)
     for key,value in speech.items():
-        if key == "nation":
-            print(filename) # Output: Nomination_Chirac1, Nomination_Chirac2, Nomination_Hollande, Nomination_Macron, Nomination_Mitterrand2
-            if max < value:
-                max = value
-                president = (max,filename)
-print(president) # Output : (4, 'Nomination_Chirac2')  like it's Chirac no need to sum his 2 speeches because he is already the one who say it the more
-"""
+        if value > max:
+            max = value
+            word= (key,value)
+        if value == max: #in order to verify if there no other value which equal to the gratest one
+            print(value)
+    print(word) #output ('de', 97)
+
+#4
+elif feature==4:
+    max=0
+    count = 0
+    president = ()
+    for filename in os.listdir("Cleaned"):
+        with open(f"Cleaned/{filename}", "r", encoding="utf-8") as f:
+            speech = f.read()
+        speech = tf(speech)
+        for key,value in speech.items():
+            if key == "nation":
+                print(filename.split("_")[1]) # Output: Nomination_Chirac1, Nomination_Chirac2, Nomination_Hollande, Nomination_Macron, Nomination_Mitterrand2
+                if max < value:
+                    max = value
+                    president = (max,filename)
+    print(president) # Output : (4, 'Nomination_Chirac2')  like it's Chirac no need to sum his 2 speeches because he is already the one who say it the more
+
 
 #5.
-"""for filename in os.listdir("Cleaned"):
-    with open(f"Cleaned/{filename}", "r", encoding="utf-8") as f:
-        speech = f.read()
-    speech = tf(speech)
-    for key in speech.keys():
-        if key == "climat" or key == "écologie":
-            print(filename) # Output: Nomination_Macron he is the only one thus he is at same the time the first one """
+elif feature==5:
+    for filename in os.listdir("Cleaned"):
+        with open(f"Cleaned/{filename}", "r", encoding="utf-8") as f:
+            speech = f.read()
+        speech = tf(speech)
+        for key in speech.keys():
+            if key == "climat" or key == "écologie":
+                print(filename.split("_")[1]) # Output: Nomination_Macron he is the only one thus he is at same the time the first one """
 #6
-all_speeches = ""
-for filename in os.listdir("Cleaned"):
-    with open(f"Cleaned/{filename}", "r", encoding="utf-8") as f:
-        speech = f.read()
-    all_speeches = all_speeches + speech
+elif feature==6:
+    all_speeches = ""
+    for filename in os.listdir("Cleaned"):
+        with open(f"Cleaned/{filename}", "r", encoding="utf-8") as f:
+            speech = f.read()
+        all_speeches = all_speeches + speech
 
-all_speeches= set(list(all_speeches.split(" ")))
-dict_of_unimportant = dict()
-for filename in os.listdir("Cleaned"):
-    with open(f"Cleaned/{filename}", "r", encoding="utf-8") as f:
-        speech = f.read()
-    speech = tf(speech)
-    for a in all_speeches:
-        if a in speech:
-            dict_of_unimportant[a] += 1
-print(dict_of_unimportant)
+    all_speeches= set(list(all_speeches.split(" ")))
+    dict_of_unimportant = dict()
+    for filename in os.listdir("Cleaned"):
+        with open(f"Cleaned/{filename}", "r", encoding="utf-8") as f:
+            speech = f.read()
+        speech = tf(speech)
+        for a in all_speeches:
+            if a in speech:
+                dict_of_unimportant[a] += 1
+    print(dict_of_unimportant)
 
-
+else:
+    print("this feature does not exist")
 
