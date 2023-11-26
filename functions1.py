01d9a44fa523e1133c07c2fd1988fbba9fd69899
Paul Gournay <151144120+PaulGournay@users.noreply.github.com>
2023-11-26 13:03:11 +0100

    Update functions1.py

--- functions1.py
+++ functions1.py
@@ -45,61 +45,67 @@ def lower_letter(file_name):
 def tf(string):
     dictionary = dict()
     string = list(string.split(" "))
-    key_word = set(string)
+    key_word = set(string) #use a set to have only one time each word
     for i in key_word:
         count = 0
         for j in string:
             if j == i:
                 count += 1
             dictionary[i] = count
-    del dictionary[""]
+    del dictionary[""] # delete element with keys nothing (little bug corrected)
 
     return dictionary
 
 
 def idf(directory):
-    num_files = 8  # like the number of files is constatn let's initialaze now
+    num_files = 8  # like the number of files is constant let's initialise it now
     all_speeches = ""
-    dico_score = defaultdict(int)
+    dico_score = defaultdict(int) #use of "collection" module tu be able to inisialize integer in the dictionarie to then be able to increment 
     for filename in os.listdir(directory):
         with open(f"{directory}/{filename}", "r", encoding="utf-8") as f:
             speech = f.read()
         all_speeches += speech
-    words = set(tf(all_speeches).keys())
+    words = set(tf(all_speeches).keys()) # use a set of all speeches getting the words of them calling the tf function to be able to use them as keys for the dico score
     for filename in os.listdir(directory):
-        with open(f"{directory}/{filename}", "r", encoding="utf-8") as f:
+        with open(f"{directory}/{filename}", "r", encoding="utf-8") as f: # loop to read the content of one file by one
             speech = f.read()
-        speech = tf(speech).keys()
+        speech = tf(speech).keys() # get all the word in the file using keys of the tf function
         for a in words:
-            if a in speech:
+            if a in speech: #read all words in a file calling the keys of tf function to check if a word in all words in the folder is in the document
                 dico_score[a] += 1
-    for key, value in dico_score.items():
+    for key, value in dico_score.items(): # create the dictionary associating the IDF score with each word
         dico_score[key] = round(math.log10(num_files / value) + 1, 5)
     return dico_score
 
 
 def Tf_idf(directory):
     score_tf_idf = dict()
-    list_speech=[]
+    list_of_dict_tf_idf=[]
     count=0
+    all_speeches =""
     list_score = []
+    TF_IDF_matrix=[]
     idf_score = idf(directory)
     for filename in os.listdir(directory):
         score_tf_idf = dict()
         with open(f"{directory}/{filename}", "r", encoding="utf-8") as f:
             speech = f.read()
-        speech = tf(speech)
+            all_speeches += speech
+        speech = tf(speech) # create a set of all
         for key, value in speech.items():
-            score_tf_idf[key] = value * idf_score[key]
-        list_speech.append(score_tf_idf)
-    return list_speech
-    """for i in range(0,len(list_speech)):
-        count=0
-        dicot =list_speech[i]
-        for key,val in dicot.items():
-            count +=1
-            list_score[count][i] = key
-"""
+            score_tf_idf[key] = value * idf_score[key] # computation of the td_idf score
+        list_of_dict_tf_idf.append(score_tf_idf) # create a list of dictionaries where each row represent the tf_idf score for each word of each document of the folder
+    words = set(tf(all_speeches).keys()) # set of all words to be able to know all the words in all documents and then use them as key to create the TF_IDF_matrix
+    for i in range(len(list_of_dict_tf_idf)):
+        for a in words:
+            if a in list_of_dict_tf_idf[i].keys():
+                TF_IDF_matrix.append(list_of_dict_tf_idf[i][a])
+
+    return(TF_IDF_matrix,list_of_dict_tf_idf)
+    
+
+
+
 
 
 
