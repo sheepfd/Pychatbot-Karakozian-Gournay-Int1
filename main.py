1c294af8c81a11f66ff15f9b72645c731e5ad350
Paul Gournay <151144120+PaulGournay@users.noreply.github.com>
2023-12-15 23:17:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from functions1 import *
 question = "bonjour je m'appelle jean je ta si ok non si france " #input("Write your question : ")
-print(len(TF_IDF_question(question)))
-print(TF_IDF_question(question))
+name_of_files = real_list_of_file("./Speeches")
+mat_of_dict=Tf_idf("cleaned")
+mat_of_dict.append(TF_IDF_question_dico(question))
+print(mat_of_dict)
+
+mat=[]
+for i in range(len(mat_of_dict)):
+    row=[]
+    for key,val in mat_of_dict[len(mat_of_dict)-1].items():
+        if key in mat_of_dict[i]:
+            row.append(mat_of_dict[i][key])
+        else:
+            row.append(0)
+    mat.append(row)
 
