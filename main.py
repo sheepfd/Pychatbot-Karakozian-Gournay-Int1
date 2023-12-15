6d86372d8bb6108babcf42472c69ecfeee6b2dd8
Paul Gournay <151144120+PaulGournay@users.noreply.github.com>
2023-12-15 11:43:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from functions1 import *
-question = input("Write your question : ")
-question = set(question)
-
-set_all_speeches = set(list(all_speech("Cleaned").split(" ")))
-print(question - set_all_speeches)
-
+question = "bonjour je m'appelle jean je ta si ok non si france " #input("Write your question : ")
+print(len(TF_IDF_question(question)))
+print(TF_IDF_question(question))
 
