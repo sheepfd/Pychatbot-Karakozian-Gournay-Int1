f28103dcc782532ac7cf8db5c9829821fc18cb57
Paul Gournay <151144120+PaulGournay@users.noreply.github.com>
2023-12-16 13:40:41 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -2,7 +2,7 @@ from functions1 import *
 
 """with open('Cleaned/Nomination_Chirac1',"r",encoding="utf-8") as f :
     speech = f.read()"""
-print(lower_letter("mlkqdsfjkmqslf lksqfdjmqdsflj mkqsfdlmsâdflj sdsf'mklfjrmmklaezjmeazkljmlkezr 'fdmslkjf qsdmfkjqdfsml O' ''' HJDOFH MDJoéklfm"))
+"""print(lower_letter("mlkqdsfjkmqslf lksqfdjmqdsflj mkqsfdlmsâdflj sdsf'mklfjrmmklaezjmeazkljmlkezr 'fdmslkjf qsdmfkjqdfsml O' ''' HJDOFH MDJoéklfm"))"""
 a = list_of_files("./Speeches","txt")
 for filename in os.listdir("Speeches"):
     Cleanedfile(f'Speeches/{filename}') # in order to fill completly the folder Cleaned
@@ -11,5 +11,7 @@ print(len(TF_IDF_matrix("Cleaned")))
     content= f.read()
     print(content)"""
 """print(TF_IDF_question(""))"""
-print(Tf_idf('Cleaned'))
-print(TF_IDF_question(""))
+"""print(Tf_idf('Cleaned'))
+print(TF_IDF_question(""))"""
+
+print(dico_text_ini('Speeches'))
