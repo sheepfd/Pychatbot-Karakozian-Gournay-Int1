bd06c6215c7f42e3a3c4cfa8a9706fbd182e2ef9
sheepfd <61257930+sheepfd@users.noreply.github.com>
2023-12-18 23:24:54 +0100

    Update Test.py

--- Test.py
+++ Test.py
@@ -1,3 +1,11 @@
+"""
+name of the project : Pychatbot-Karakozian-Gournay-Int1
+authors : Paul Gournay, Vrej Karakozian
+role this file : as debuger
+comment :  in that file we used it as debuger with print in order to show what we were doing in the console, And also we use it to change our files in speeches to put them in cleaned .
+"""
+
+
 from functions1 import *
 
 """with open('Cleaned/Nomination_Chirac1',"r",encoding="utf-8") as f :
